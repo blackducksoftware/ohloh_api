42ca30b03a7fa4ef882607841f480f9c8794e26a
Alex Johnson <notalexjohnson@gmail.com>
2015-05-19 16:42:44 +0530

    OTWO-3498: Add example and instructions for OAuth2

---
+++ examples/oauth2/ohloh_oauth2_sinatra.rb
@@ -0,0 +1,29 @@
+require 'oauth2'
+require 'sinatra'
+
+enable :sessions
+CALLBACK_URL = 'http://localhost:9292/callback'
+
+# Assumes that you have set OHLOH_KEY and OHLOH_SECRET environment
+# variables to values from your API keys page.
+before do
+  @client = OAuth2::Client.new(ENV['OHLOH_KEY'], ENV['OHLOH_SECRET'],
+                               site: 'https://www.openhub.net',
+                               token_method: :post)
+end
+
+def access_token
+  OAuth2::AccessToken.new(@client, session[:access_token], refresh_token: session[:refresh_token])
+end
+
+get '/login' do
+  redirect @client.auth_code.authorize_url(redirect_uri: CALLBACK_URL)
+end
+
+get '/callback' do
+  new_token = @client.auth_code.get_token(params[:code], redirect_uri: CALLBACK_URL)
+  session[:access_token] = new_token.token
+  session[:refresh_token] = new_token.refresh_token
+
+  access_token.get('accounts/me.xml').body
+end
